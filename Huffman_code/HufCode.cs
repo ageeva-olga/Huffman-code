9b1a0ce5608023ee5444b4e69a44b797f10d3db8
Olga Ageeva <olga.ageeva.999@mail.ru>
2019-09-12 10:07:23 +0300

    Добавила сохранение в файл текста

---
+++ Huffman_code/HufCode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Huffman_code
+{
+    public class HufCode
+    {
+            public string Text { get; set; }
+            public HufCode(string text)
+            {
+                Text = text;
+            }
+            public void Save(string path)
+            {
+                using (var writetext = new StreamWriter(path))
+                {
+                    writetext.WriteLine(string.Format(Text));
+                }
+            }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Huffman_code;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SaveTest()
+        {
+            var filePath = Path.Combine("c:\\temp", "text1.txt");
+            var text = new HufCode("Идея, положенная в основу кодировании Хаффмана, " +
+                                   "основана на частоте появления символа в последовательности.");
+            text.Save(filePath);
+        }
+    }
+}
