09acd5609ba98b35ef3df9a1c7e271746ee2eafd
Olga Ageeva <olga.ageeva.999@mail.ru>
2019-10-20 18:24:15 +0300

    Добавила восстановление строки битов из байтов, исправила метод формирования байтов

--- Huffman_code/HufCode.cs
+++ Huffman_code/HufCode.cs
@@ -99,7 +99,7 @@ namespace Huffman_code
                 hufTable.Add(tree.Value.Value, code);
         }
 
-        public byte[] GetBynary(Dictionary<char, string> table)
+        public byte[] GetBynary(Dictionary<char, string> table, out int tail)
         {
             var bytesString = "";
             for(int i = 0; i<Text.Length;i++)
@@ -107,12 +107,45 @@ namespace Huffman_code
                 bytesString+= table[Text[i]];
             }
 
-            int nBytes = bytesString.Length / 8;
+            tail = 8 - bytesString.Length % 8;
+            for (int i=0; i< tail; i++)
+            {
+                bytesString += "0";
+            }
+
+            int nBytes =(int)Math.Ceiling((double)bytesString.Length / 8);
             var bytesAsStrings =
                 Enumerable.Range(0, nBytes)
                           .Select(i => bytesString.Substring(8 * i, 8));
             byte[] bytes = bytesAsStrings.Select(s => Convert.ToByte(s, 2)).ToArray();
             return bytes;
+        }
+        public string RestorBynaryString(byte[] bytes, int tail)
+        {
+            var bitString = "";
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                string bits = Convert.ToString(bytes[i], 2);
+                if(bits.Length<8)
+                {
+                    var fillCount = 8 - bits.Length;
+                    for (int j = 0; j< fillCount; j++)
+                    {
+                        bits = "0" + bits;
+                    }
+                }
+                if(i== bytes.Length-1 && tail != 0)
+                {
+                    bits = bits.Substring(0, 8 - tail);
+                }
+
+                bitString += bits;
+            }
+            return bitString;
+        }
+        public string RestoreText(Node tree, string bits)
+        {
+
         }
     }
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -74,9 +74,45 @@ namespace UnitTestProject1
             var table = huffCode.MakeHuffmanTable();
             var tree = huffCode.MakeHuffmanTree(table);
             var treeCode = huffCode.GetHuffmanTable(tree);
-            var byteArray = huffCode.GetBynary(treeCode);
+            int tail = 0;
+
+            var byteArray = huffCode.GetBynary(treeCode, out tail);
             Assert.AreEqual(187, byteArray[0]);
             Assert.AreEqual(18, byteArray[1]);
+            Assert.AreEqual(224, byteArray[2]);
+            Assert.AreEqual(5, tail);
+        }
+
+        [TestMethod]
+        public void RestoreBytesTest()
+        {
+            var huffCode = new HufCode();
+            var bytes = new byte[3] { 187, 18, 224 };
+            var bit = huffCode.RestorBynaryString(bytes, 5);
+            Assert.AreEqual("1011101100010010111", bit);
+        }
+
+        [TestMethod]
+        public void RestoreTextTest()
+        {
+            var bits = "1011101100010010111";
+            var huffCode = new HufCode();
+            var tree = new Node()
+            {
+                LeftNode = new Node() { Value = 'b' },
+                RightNode = new Node()
+                {
+                    LeftNode = new Node()
+                    {
+                        LeftNode = new Node() { Value = 'd' },
+                        RightNode = new Node() { Value = 'c'}
+                    },
+                    RightNode = new Node() { Value = 'a' }
+                }
+            };
+
+            var text = huffCode.RestoreText(tree, bits);
+            Assert.AreEqual("cababbbdca", text);
         }
     }
 }
